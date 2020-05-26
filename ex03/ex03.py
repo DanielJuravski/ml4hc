7aa1e5ab3ab1869c5b6dd1a73fded1520befa0e7
Daniel Juravski <juravski95@gmail.com>
2020-05-26 12:21:47 +0300

    done up to part 5

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -32,21 +32,32 @@ def init_data(data):
     return train_x, test_x, train_y, test_y
 
 
-def q01(train_x, test_x, train_y, test_y):
-    parameters = {'n_estimators': (10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 15000),
+def q01_02(train_x, test_x, train_y, test_y):
+    parameters = {'n_estimators': (10, 20, 50, 100, 200, 500),
                   'max_depth': (2, 3, 4, 5, 6, 7, 8)
                   }
     parameters = {'n_estimators': (20, 50),
                   'max_depth': (4, 6, 8)
                   }
+    clf = ensemble.RandomForestClassifier(random_state=1)
+    classifiers = GridSearchCV(clf, parameters, scoring='accuracy', return_train_score=True)
+    classifiers.fit(train_x, train_y)
+
+    train_process_results = pd.DataFrame(list(zip(classifiers.cv_results_['params'],
+                                                  classifiers.cv_results_['mean_train_score'],
+                                                  classifiers.cv_results_['mean_test_score'])),
+                                         columns=["Parameter", "mean train score", "mean validation score"])
+    print(train_process_results)
+    print(classifiers.best_params_)
+
     clf = ensemble.RandomForestClassifier(random_state=2)
     classifiers = GridSearchCV(clf, parameters, scoring='accuracy', return_train_score=True)
     classifiers.fit(train_x, train_y)
 
     train_process_results = pd.DataFrame(list(zip(classifiers.cv_results_['params'],
-                          classifiers.cv_results_['mean_train_score'],
-                          classifiers.cv_results_['mean_test_score'])),
-                 columns=["Parameter", "mean train score", "mean validation score"])
+                                                  classifiers.cv_results_['mean_train_score'],
+                                                  classifiers.cv_results_['mean_test_score'])),
+                                         columns=["Parameter", "mean train score", "mean validation score"])
     print(train_process_results)
     print(classifiers.best_params_)
 
@@ -56,19 +67,6 @@ def q01(train_x, test_x, train_y, test_y):
     print("Test data acc: {0}".format(y_test_pred))
 
     best_rf = classifiers.best_estimator_
-
-
-    # X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
-    # y = np.array([0, 0, 1, 1])
-    # skf = StratifiedKFold(n_splits=2, random_state=None, shuffle=False)
-    # print(len(skf))
-    #
-    # print(skf)
-    #
-    # for train_index, test_index in skf:
-    #     print("TRAIN:", train_index, "TEST:", test_index)
-    #     X_train, X_test = X[train_index], X[test_index]
-    #     y_train, y_test = y[train_index], y[test_index]
 
     return best_rf
 
@@ -85,17 +83,17 @@ def q03(best_rf, train_x):
         print("%d. feature %s (%f)" % (i, train_x.columns.values[indices[i]], importances[indices[i]]))
 
     # # Plot the impurity-based feature importances of the forest
-    import matplotlib.pyplot as plt
-    plt.rcParams["figure.figsize"] = (14, 6)
-    plt.rcParams["font.size"] = 16
-    plt.rcParams["lines.linewidth"] = 2.5
-
-    plt.figure()
-    plt.title("Feature importances")
-    plt.bar(range(train_x.shape[1]), importances[indices], color="r", align="center")
-    plt.xticks(range(train_x.shape[1]), [train_x.columns.values[i] for i in indices], rotation=90)
-    plt.xlim([-1, train_x.shape[1]])
-    plt.show()
+    # import matplotlib.pyplot as plt
+    # plt.rcParams["figure.figsize"] = (14, 6)
+    # plt.rcParams["font.size"] = 16
+    # plt.rcParams["lines.linewidth"] = 2.5
+    #
+    # plt.figure()
+    # plt.title("Feature importances")
+    # plt.bar(range(train_x.shape[1]), importances[indices], color="r", align="center")
+    # plt.xticks(range(train_x.shape[1]), [train_x.columns.values[i] for i in indices], rotation=90)
+    # plt.xlim([-1, train_x.shape[1]])
+    # plt.show()
 
 
 def q04(best_rf, test_x):
@@ -103,7 +101,14 @@ def q04(best_rf, test_x):
     import warnings
     warnings.filterwarnings('ignore')
 
-    shap.KernelExplainer(best_rf.predict, test_x).shap_values(test_x)
+    shap_values = shap.KernelExplainer(best_rf.predict, test_x).shap_values(test_x)
+    shap.summary_plot(shap_values, test_x)
+
+    shap_sum = np.abs(shap_values).mean(axis=0)
+    importance_df = pd.DataFrame([test_x.columns.tolist(), shap_sum.tolist()]).T
+    importance_df.columns = ['column_name', 'shap_importance']
+    importance_df = importance_df.sort_values('shap_importance', ascending=False)
+    print(importance_df)
 
 
 def q05(best_rf, test_x):
@@ -111,13 +116,18 @@ def q05(best_rf, test_x):
     shap_values = shap_values[1]  # shap values of the 1-class
     shap.summary_plot(shap_values, test_x, plot_type="dot")
 
+    shap_sum = np.abs(shap_values).mean(axis=0)
+    importance_df = pd.DataFrame([test_x.columns.tolist(), shap_sum.tolist()]).T
+    importance_df.columns = ['column_name', 'shap_importance']
+    importance_df = importance_df.sort_values('shap_importance', ascending=False)
+    print(importance_df)
+
 
 if __name__ == '__main__':
     data = load_data()
     train_x, test_x, train_y, test_y = init_data(data)
 
-    best_rf = q01(train_x, test_x, train_y, test_y)
-    # q02
+    best_rf = q01_02(train_x, test_x, train_y, test_y)
     q03(best_rf, train_x)
     q04(best_rf, test_x)
     q05(best_rf, test_x)
